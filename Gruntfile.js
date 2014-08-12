3e20f9d7ad2e5278f73c91773da5853380b86927
frank hadder <luckyllama@gmail.com>
2014-08-12 15:14:11 -0700

    added default assemble structure

---
+++ Gruntfile.js
@@ -0,0 +1,131 @@
+/*
+ * Generated on 2014-08-12
+ * generator-assemble v0.4.13
+ * https://github.com/assemble/generator-assemble
+ *
+ * Copyright (c) 2014 Hariadi Hinta
+ * Licensed under the MIT license.
+ */
+
+"use strict";
+
+// # Globbing
+// for performance reasons we"re only matching one level down:
+// "<%= config.src %>/templates/pages/{,*/}*.hbs"
+// use this if you want to match all subfolders:
+// "<%= config.src %>/templates/pages/**/*.hbs"
+
+module.exports = function(grunt) {
+
+  require("time-grunt")(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    config: {
+      src: "src",
+      dist: "dist"
+    },
+
+    watch: {
+      assemble: {
+      files: ["<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml}"],
+        tasks: ["assemble"]
+      },
+      livereload: {
+        options: {
+          livereload: "<%= connect.options.livereload %>"
+        },
+        files: [
+            "<%= config.dist %>/{,*/}*.html",
+            "<%= config.dist %>/assets/{,*/}*.css",
+            "<%= config.dist %>/assets/{,*/}*.js",
+            "<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}"
+        ]
+      }
+    },
+
+    // css
+	less: {
+		development: {
+			options: {
+				compress: true,
+				sourceMap: true,
+				sourceMapFilename: "<%= config.dist %>/assets/css/main.css.map",
+				sourceMapURL: "<%= config.dist %>/css/main.css.map",
+				sourceMapRootpath: "/"
+			},
+			files: {
+				// target.css file: source.less file
+				"<%= config.dist %>/assets/css/main.css": "<%= config.src %>/assets/css/main.less"
+			}
+		}
+	},
+	autoprefixer: {
+		options: { },
+		files: {
+			src: "<%= config.dist %>/assets/css/main.css"
+		},
+	},
+
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to "0.0.0.0" to access the server from outside
+        hostname: "localhost"
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            "<%= config.dist %>"
+          ]
+        }
+      }
+    },
+
+    assemble: {
+      pages: {
+        options: {
+          flatten: true,
+          assets: "<%= config.dist %>/assets",
+          layout: "<%= config.src %>/templates/layouts/default.hbs",
+          data: "<%= config.src %>/data/*.{json,yml}",
+          partials: "<%= config.src %>/templates/partials/*.hbs",
+          plugins: ["assemble-contrib-permalinks","assemble-contrib-sitemap"],
+        },
+        files: {
+          "<%= config.dist %>/": ["<%= config.src %>/templates/pages/*.hbs"]
+        }
+      }
+    },
+
+    // Before generating any new files,
+    // remove any previously-created files.
+    clean: ["<%= config.dist %>/**/*.{html,xml}"]
+
+  });
+
+  grunt.loadNpmTasks("assemble");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.registerTask("server", [
+    "clean",
+    "assemble",
+    "connect:livereload",
+    "watch"
+  ]);
+
+  grunt.registerTask("build", [
+    "clean",
+    "assemble"
+  ]);
+
+  grunt.registerTask("default", [
+    "build"
+  ]);
+
+};
