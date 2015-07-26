ef9d620fc70ec3ed2eb0f1b4f697d45e495dc85d
luckyllama <luckyllama@gmail.com>
2015-07-26 16:56:01 -0700

    added code to load articles via ajax

--- src/assets/scripts/app/menu.js
+++ src/assets/scripts/app/menu.js
@@ -5,4 +5,32 @@
       event.preventDefault();
       $("body").toggleClass("menu-open");
    });
+
+   var getContent = function (url) {
+      $.ajax({ url: url.replace("html", "json.js"), dataType: "json" })
+         .success(function (data) {
+            if (!data || !data.content) { window.location.href = url; }
+            $("main").html(data.content);
+            window.history.pushState({ url: url }, data.title, url);
+            document.title = data.title;
+         })
+         .error(function () { window.location.href = url; });
+   };
+
+   if (window.history.pushState) {
+      $(document).on("click", "a", function (event) {
+         var url = $(event.target).attr("href");
+         if (url.indexOf("http") < 0) {
+            getContent(url);
+         }
+         event.preventDefault();
+      });
+      $(window).on("popstate", function (event) {
+         var url = window.location.pathname;
+         if (event.originalEvent.state && event.originalEvent.state.url) {
+            url = event.originalEvent.state.url;
+         }
+         getContent(url);
+      });
+   }
 })(window.jQuery);
